04115230227aa5f5de8a29e2dc9494e1becf45f4
Carl Chen <ccheartszzz@gmail.com>
2025-04-06 01:06:01 +0800

    feat: optimize logger

--- lib/error-handler.ts
+++ lib/error-handler.ts
@@ -1,13 +1,27 @@
 import { isFn, isObject, isStr } from './validate'
 
+/**
+ * Formats an error object into a string representation.
+ * @param error - The error to format. It can be an `Error` instance, a string, or another object.
+ * @param [defaultErrorString=''] - The default error message if the error cannot be formatted.
+ * @param [opts={}] - Additional options.
+ * @param [opts.errorLimit=8] - The maximum number of stack trace lines to include.
+ * @returns  The formatted error message.
+ */
 export function formatErrorToString(
   error: unknown,
-  defaultErrorString: string = ''
+  defaultErrorString: string = '',
+  opts = {
+    errorLimit: 8
+  }
 ) {
+  if (error instanceof Error) {
+    Error.captureStackTrace(error, formatErrorToString)
+    const stackLines = error.stack?.split('\n').slice(0, opts.errorLimit) || []
+    return stackLines.join('\n')
+  }
+
   if (isStr(error)) return error
-
-  if (error instanceof Error) return error.message
-
   if (isObject(error) && 'toString' in error && isFn(error.toString))
     return error.toString()
 
--- lib/log/index.ts
+++ lib/log/index.ts
@@ -1,23 +1,29 @@
 const LOG_LEVELS = {
+  START: -2,
+  SUCCESS: -1,
   ERROR: 0,
   WARN: 1,
-  NORMAL: 2,
+  INFO: 2,
   DEBUG: 3,
   TRACE: 4
 }
 
 const ANSI_COLORS = {
+  [LOG_LEVELS.START]: '\x1b[32m', // green
+  [LOG_LEVELS.SUCCESS]: '\x1b[32m', // green
   [LOG_LEVELS.ERROR]: '\x1b[31m', // red
   [LOG_LEVELS.WARN]: '\x1b[33m', // yellow
-  [LOG_LEVELS.NORMAL]: '\x1b[32m', // green
+  [LOG_LEVELS.INFO]: '\x1b[32m', // green
   [LOG_LEVELS.DEBUG]: '\x1b[34m', // blue
   [LOG_LEVELS.TRACE]: '\x1b[35m' // purple
 }
 
 const LOG_LEVEL_NAMES = {
+  [LOG_LEVELS.START]: 'success',
+  [LOG_LEVELS.SUCCESS]: 'success',
   [LOG_LEVELS.ERROR]: 'error',
   [LOG_LEVELS.WARN]: 'warn',
-  [LOG_LEVELS.NORMAL]: 'info',
+  [LOG_LEVELS.INFO]: 'info',
   [LOG_LEVELS.DEBUG]: 'debug',
   [LOG_LEVELS.TRACE]: 'trace'
 }
@@ -50,37 +56,45 @@ export class Logger {
     this.level = level
   }
 
-  log(level: number, message: string) {
+  log(level: number, ...message: string[]) {
     if (level > this.level) {
       return
     }
-    const color = ANSI_COLORS[level] || ANSI_COLORS[LOG_LEVELS.NORMAL]
+    const color = ANSI_COLORS[level] || ANSI_COLORS[LOG_LEVELS.INFO]
     const levelName: string = LOG_LEVEL_NAMES[level]
     const icon = Reflect.get(TYPE_ICONS, levelName) || ''
     const timestamp = new Date().toLocaleString()
     const logMethod = level === LOG_LEVELS.ERROR ? 'error' : 'log'
     console[logMethod](
-      `${color}${icon}\x1b[0m [${levelName}] (${timestamp}) ${message}`
+      `${color}${icon}\x1b[0m [${levelName}] (${timestamp}) :`, ...message
     )
   }
 
-  error(message: string) {
-    this.log(LOG_LEVELS.ERROR, message)
+  error(...message: string[]) {
+    this.log(LOG_LEVELS.ERROR, ...message)
   }
 
-  warn(message: string) {
-    this.log(LOG_LEVELS.WARN, message)
+  warn(...message: string[]) {
+    this.log(LOG_LEVELS.WARN, ...message)
   }
 
-  info(message: string) {
-    this.log(LOG_LEVELS.NORMAL, message)
+  info(...message: string[]) {
+    this.log(LOG_LEVELS.INFO, ...message)
   }
 
-  debug(message: string) {
-    this.log(LOG_LEVELS.DEBUG, message)
+  debug(...message: string[]) {
+    this.log(LOG_LEVELS.DEBUG, ...message)
   }
 
-  trace(message: string) {
-    this.log(LOG_LEVELS.TRACE, message)
+  trace(...message: string[]) {
+    this.log(LOG_LEVELS.TRACE, ...message)
+  }
+
+  start(...message: string[]) {
+    this.log(LOG_LEVELS.START, ...message)
+  }
+
+  success(...message: string[]) {
+    this.log(LOG_LEVELS.SUCCESS, ...message)
   }
 }
